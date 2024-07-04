282936aa12164a6b02788766213afe96d5ccff61
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-07-04 14:50:59 +0200

    Changed & Added
    Changed - Simplified searchByParams fn
    
    Added:
    1 - getOne fn with route
    2 - delete fn  with route

--- controllers/apparts.js
+++ controllers/apparts.js
@@ -6,7 +6,7 @@ const addSchemaJoiApparts = require("../schemaJOI/apparts");
 const getAllApparts = tryCatch(async (req, res) => {
   const result = await Apparts.find();
   if (result.length === 0) {
-    console.log("is empty");
+    throw HttpError(500, "Is empty");
   } else {
     res.json(result).status(200);
   }
@@ -23,49 +23,60 @@ const addAppart = tryCatch(async (req, res) => {
     return res.status(400).json({ error: "Item has been upload" });
   }
   const newAppart = await Apparts.create(req.body);
+  if (!newAppart) {
+    throw HttpError(500, "Somethink went wrong. Please try later :(");
+  }
   res.status(201).json(newAppart);
 });
 
-
-
-
-
 const searchByParams = tryCatch(async (req, res) => {
-    const { location, price, hasBalcony } = req.query; 
+  const { location, price, hasBalcony } = req.query;
 
+  let conditions = {};
+
+  if (location) {
+    conditions.location = location;
+  }
+  if (price) {
+    conditions.price = { $lte: parseInt(price, 10) };
+  }
+
+  if (hasBalcony !== undefined) {
+    let parseBalcony =
+      typeof hasBalcony === "string"
+        ? JSON.parse(hasBalcony.toLowerCase())
+        : !!hasBalcony;
+    conditions.hasBalcony = parseBalcony;
+  }
+
+  const result = await Apparts.find(conditions);
+
+  if (!result || result.length === 0) {
+    throw HttpError(404, "Nothing found :(");
+  }
+
+  res.json(result).status(200);
+});
+
+const getOneAppart = tryCatch(async (req, res) => {
+  const { id } = req.params;
+  const result = await Apparts.findById({ _id: id });
+
+  if (!result || result.length === 0) {
+    throw HttpError(404, "Nothing found :(");
+  }
+  res.json(result).status(200);
+});
+
+const deleteOneAppart = tryCatch(async(req, res)=>{
+    const { id } = req.params;
+
+    const result = await Apparts.findByIdAndDelete({_id: id})
     
-    let parseBalcony;
-    let conditions = {};
-  
-    if (location) {
-      conditions.location = location;
-    }
-    if (price !== undefined) {
-        conditions.price = { $lt: parseInt(price, 10) };
-    }
-    
-    if (typeof hasBalcony === 'string') {
-        parseBalcony = JSON.parse(hasBalcony.toLowerCase());
-    } else if (typeof hasBalcony === 'undefined') {
-    } 
-     else  {
-        parseBalcony = !!hasBalcony; 
+    if (!result) {
+        throw HttpError(500, 'Somethink went wrong :(' )
     }
 
-    if (typeof parseBalcony === 'boolean') {
-        conditions.hasBalcony = parseBalcony;
-    }
-    
-    console.log('Условия поиска:', conditions);
-  
-    const result = await Apparts.find(conditions);
-  
-    console.log('Результаты поиска:', result);
-    
-    res.json(result);
-  });
-  
-  
-
-
-module.exports = { getAllApparts, addAppart, searchByParams };
+    res.json(200, "Succsess deleted :)")
+})
+module.exports = { getAllApparts, addAppart, searchByParams, getOneAppart, deleteOneAppart };
--- routes/api/appartItem.js
+++ routes/api/appartItem.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
-const {getAllApparts, addAppart,searchByParams } = require("../../controllers/apparts");
+const {getAllApparts, addAppart,searchByParams, getOneAppart,deleteOneAppart } = require("../../controllers/apparts");
 
 router.get('/', getAllApparts);
 router.post('/add', addAppart);
 router.get("/search", searchByParams);
+router.get('/:id', getOneAppart)
+router.delete('/:id', deleteOneAppart)
 
 module.exports = router;
